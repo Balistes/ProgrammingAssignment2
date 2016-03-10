29ad18cc99bcdf5f689231b811c626ec4804e65d
Balistes <balistes@me.com>
2016-03-11 01:35:14 +0300

    Update program function for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,10 @@
-## Function makeCacheMatrix and cacheSolve use to computing the inverse of a square matrix
-## with the R function solve.
-## The following functions work together to create invertible matrix
+## Functions "makeCacheMatrix" and "cacheSolve" use to computing the inverse of a square matrix.
+## This functions work together to create invertible matrix
 ## and make the inverse of the matrix available in the cache environment.
+## R function "solve" is used to obtain the inverse matrix.
 
 ## Function makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 ## makeCacheMatrix creates and returns a list of functions
-
 makeCacheMatrix <- function(x = matrix()) {
      m <- NULL
      set <- function(y) {
@@ -23,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Function cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 ## If the inverse has already been calculated (and the matrix has not changed), 
 ## then cacheSolve retrieve the inverse from the cache.
-## Function returns a invertible matrix or, if matrix is not square, returns NA.
+## Function returns a inverse matrix.
 
 cacheSolve <- function(x, ...) {
     m <- x$getinv()
